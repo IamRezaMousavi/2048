847c73888114284cb3bad1a60d202fa91def04db
Reza Mousavi <iamrezamousavi@gmail.com>
2023-08-02 03:48:04 +0330

    testing 2

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + TS</h1>
+      <h2>My name is Reza</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
