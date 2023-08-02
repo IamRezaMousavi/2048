a9f37c0cdc986d6a32f5f31e197c33680adfa426
Reza Mousavi <iamrezamousavi@gmail.com>
2023-08-02 05:19:40 +0330

    add bootstrap for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import NavBar from './components/NavBar';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
