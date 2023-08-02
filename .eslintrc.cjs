339034b719ad2d1ac2610b2f8cfce0d3d05cba32
Reza Mousavi <iamrezamousavi@gmail.com>
2023-08-02 15:28:46 +0330

    add react eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,33 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'react',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'react/react-in-jsx-scope': ['off']
   },
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap-utilities.min.css';
 import './NavBar.css';
 
 const NavBar: React.FC = () => {
-    return (
-        <nav className="navbar fixed-top transparent-bg">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Fixed top</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar fixed-top transparent-bg">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Fixed top</a>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
